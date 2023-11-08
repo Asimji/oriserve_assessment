97d1e5018fe0354c3f8dd1caa85d5093fe989771
Asim <mohdasim.srk@gmailcom>
2023-11-08 14:19:58 +0530

    search history debounce completed

--- ori/src/components/Homepage.jsx
+++ ori/src/components/Homepage.jsx
@@ -1,36 +1,128 @@
-import { Box, Flex, Grid, Heading, Image, Input } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react'
+import { Box, Button, Flex, Grid, Heading, Image, Input } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import debounce from 'lodash/debounce'
+import SearchModal from './SearchModal';
 
 const apiKey = '4c048c2707f265505905a851bb4b4e8f';
 const apiUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${apiKey}&format=json&nojsoncallback=1&safe_search=1`;
-
+const perPage = 20; 
+const pageToFetch = 1;
 const Homepage = () => {
-    const [photos,setPhotos]=useState([])
-    const [query,setQuery]=useState("")
+    const [photos,setPhotos]=useState([]);
+    const [query,setQuery]=useState("");
+    const [searchResult,setSearchResult]=useState([]);
+    const [page, setPage] = useState(20);
+    const [searchHistory,setSearchHistory]=useState([]);
+    const [showSuggestions, setShowSuggestions] = useState(false);
+  
+
+  
+
+    // fetching Images First Time
     useEffect(()=>{
-         fetch(`${apiUrl}`).then(res=>res.json()).then(res=>{setPhotos(res.photos.photo)}).catch(e=>console.log(e))
+         fetch(`${apiUrl}&per_page=${perPage}`).then(res=>res.json()).then(res=>{setPhotos(res.photos.photo)}).catch(e=>console.log(e))
     },[])
-    
+
  
-  return (
+    // Search Function Debounce Technique 
+    const debounceSearch=debounce((query)=>{
+      fetch(`${apiUrl}&text=${query}&per_page=${page}`).then(res=>res.json()).then(res=>{setSearchResult(res.photos.photo)}).catch(e=>console.log(e))
+    },300)
+ 
+    // Input Change Method 
+const handleChange=(e)=>{
+    setQuery(e.target.value);
+}
+const handleSubmit=()=>{
+  setSearchHistory((prev) => {
+    const updatedSearchHistory = [...prev, query];
+    console.log(query)
+    localStorage.setItem('searchDetail', JSON.stringify(updatedSearchHistory));
+    return updatedSearchHistory;
+  });
+  // Storing Search History
+  debounceSearch(query)
+}
+
+// Fetching Search History From LocalStorage
+
+useEffect(() => {
+  const savedSearchHistory = JSON.parse(localStorage.getItem('searchDetail'));
+  if (savedSearchHistory) {
+    setSearchHistory(savedSearchHistory);
+  }
+
+}, []);
+
+
+
+// For Infinite Scroll
+const handleScroll=()=>{
+  if(window.innerHeight + document.documentElement.scrollTop===document.documentElement.offsetHeight){
+      setPage((prev)=>prev+20); 
+      debounceSearch(query);
+  }
+}
+
+useEffect(()=>{
+  window.addEventListener('scroll',handleScroll)
+  return ()=> window.removeEventListener('scroll',handleScroll)
+})
+
+
+
+
+const handleClear=()=>{
+  console.log("Clear")
+ localStorage.removeItem('searchDetail')
+ setSearchHistory([])
+ setShowSuggestions(false)
+}
+
+    return (
     <Box>
        <Flex direction={'column'} alignItems={'center'} justify={'center'} bgColor={'black'} textColor={'#ffff'} p={'3vh'}>
            <Heading>Search Photos</Heading>
-           <Input placeholder='search' w={'25vw'} mt={'2vh'} onChange={(e)=>setQuery(e.target.value)}/>
+           <Flex alignItems={'center'} gap={'2vh'}>
+
+           <Input placeholder='search' w={'25vw'} mt={'2vh'} onChange={(e)=>handleChange(e)}
+            onClick={() => setShowSuggestions(true)}
+            
+            />
+            <Button onClick={handleSubmit} mt={'2vh'}>Search</Button>
+            </Flex>
+              {showSuggestions && searchHistory.length>0 && (
+     <SearchModal  searchResults={searchHistory} handleClear={handleClear} setShowSuggestions={setShowSuggestions}  />
+    )} 
+   
+
+   
+    
+    
        </Flex>
 
-       <Grid templateColumns={'repeat(8,1fr)'} gap={'2vh'} mt={'2vh'}>
+       <Grid templateColumns={{lg:'repeat(4,1fr)',base:'repeat(2,1fr)'}} gap={'2vh'} mt={'2vh'} p={'2vh'}
+
+       >
         
         
-        {photos?.map((item)=>{
-          return <Box>
+        { searchResult.length>0 ? searchResult?.map((item)=>{
+            return <Box w={'100%'}> <Image
+            key={item.id}
+            src={`https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}_q.jpg`}
+            alt={item.title}
+            w={'100%'}
+            /></Box>
+        }) : photos?.map((item)=>{
+          return <Box w={'80%'}  key={item.id}>
            <Image
-          key={item.id}
           src={`https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}_q.jpg`}
           alt={item.title}
+          w='100%'
           />
           </Box>
-        })}
+        })
+        }
        </Grid>
     </Box>
   )
---
+++ ori/src/components/SearchModal.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './search.css'; // Import the modal styles
+import {  Box, Button} from '@chakra-ui/react';
+
+const SearchModal = ({searchResults, handleClear, setShowSuggestions }) => {
+  return (
+    <Box className="dropdown" position={'absolute'}  top= {{lg:'8%',md:'4%',base:'6%'}} left= {{lg:'34%',md:'31%',base:'25%'}} >
+          {searchResults
+                  .map((item,i) => (
+              <div             
+                className="dropdown-row"
+                key={i}
+              >
+                {item}
+              </div>
+            ))}
+            <Button onClick={handleClear}  ml={{lg:'80%',md:'50%'}} w={{lg:'10vh',md:'50%',base:'30%'}}  fontSize={{md:'16px',base:'12px'}} bgColor={'red'} onBlur={() => {setShowSuggestions(false);console.log('blur')}}>Clear</Button>
+        </Box>
+      
+  );
+};
+
+export default SearchModal;
